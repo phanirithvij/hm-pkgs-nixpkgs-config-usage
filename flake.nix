443c8fff760beec08b021312c2a5743b08813693
phanirithvij <phanirithvij2000@gmail.com>
2025-04-30 10:36:13 +0530

    nixpkgs.config with home-manager
    Signed-off-by: phanirithvij <phanirithvij2000@gmail.com>

---
+++ flake.nix
@@ -0,0 +1,89 @@
+{
+  description = "allowUnfree with nixos module";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    darwin.url = "github:LnL7/nix-darwin";
+    darwin.inputs.nixpkgs.follows = "nixpkgs";
+  };
+  outputs =
+    {
+      nixpkgs,
+      home-manager,
+      darwin,
+      ...
+    }:
+    let
+      system = "x86_64-linux";
+      config = {
+        allowUnfree = true;
+      };
+      pkgs = import nixpkgs {
+        inherit system config;
+      };
+      unfreePkgs = P: [ P.python3.pkgs.python-pipedrive ];
+    in
+    {
+      nixosConfigurations.test = nixpkgs.lib.nixosSystem {
+        inherit system;
+        # inherit pkgs;
+        modules = [
+          home-manager.nixosModules.home-manager
+          { nixpkgs.config.allowUnfree = true; }
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.users.test =
+              { pkgs, ... }:
+              {
+                home.homeDirectory = "/home/test";
+                home.username = "test";
+                home.stateVersion = "25.05";
+                home.packages = unfreePkgs pkgs;
+              };
+          }
+          (
+            { pkgs, ... }:
+            {
+              fileSystems."/".device = "/dev/null";
+              boot.loader.grub.devices = [ "/dev/null" ];
+              system.stateVersion = "25.05";
+              users.users.test = {
+                isNormalUser = true;
+              };
+              environment.systemPackages = unfreePkgs pkgs;
+            }
+          )
+        ];
+      };
+      darwinConfigurations.test = darwin.lib.darwinSystem {
+        inherit pkgs;
+        modules = [
+          home-manager.darwinModules.home-manager
+          { nixpkgs.config.allowUnfree = true; }
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.users.test =
+              { pkgs, ... }:
+              {
+                # home.homeDirectory = "/Users/test";
+                home.username = "test";
+                home.stateVersion = "25.05";
+                home.packages = unfreePkgs pkgs;
+              };
+          }
+          (
+            { pkgs, ... }:
+            {
+              system.stateVersion = 6;
+              users.users.test = {
+                home = "/Users/test";
+              };
+              environment.systemPackages = unfreePkgs pkgs;
+            }
+          )
+        ];
+      };
+    };
+}
